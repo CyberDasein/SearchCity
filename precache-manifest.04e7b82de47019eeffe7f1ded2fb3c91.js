e07d7eca3bee6b5c8cfb5310bacff1c0b2c306c4
Alex <CyberDasein>
2020-05-23 18:52:40 +0500

    Updates

---
+++ precache-manifest.04e7b82de47019eeffe7f1ded2fb3c91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244b93397e3f835ec66d4f4ca073b9b6",
+    "url": "/SearchCity/index.html"
+  },
+  {
+    "revision": "21c202e17212a35d0305",
+    "url": "/SearchCity/static/js/2.b2bb1615.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/SearchCity/static/js/2.b2bb1615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98050d79c99f82938018",
+    "url": "/SearchCity/static/js/main.75858fa5.chunk.js"
+  },
+  {
+    "revision": "7b56c0a15b3154d70ac5",
+    "url": "/SearchCity/static/js/runtime-main.beae77ab.js"
+  }
+]);
